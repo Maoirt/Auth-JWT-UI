62e79a8bc875e56ff5e87583ada3d2861b18664b
Daniil Yatsiny <maoirtx77@gmail.com>
2024-12-18 19:53:59 +0300

    auth+JWT

---
+++ auth-service/src/main/java/com/example/auth_service/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.example.auth_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private UUID id;
+    private String userName;
+    private String token;
+
+}
